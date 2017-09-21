48c17dc26006aa1b80a9819bbdd55d2c7f3c14d0
Elgs Qian Chen <elgs1980@hotmail.com>
2017-09-21 03:11:40 -0700

    Added Interceptor interface.

---
+++ interceptor.go
@@ -0,0 +1,22 @@
+package wsl
+
+import "database/sql"
+
+// Interceptor provides a chance for application to gain more controls over
+// the parameters before, the result after, and the error when the query is
+// executed. An example would be to provide additional input parameters for
+// the query, or convert the result to other formats.
+type Interceptor interface {
+	Before(tx *sql.Tx, script string, params map[string]string) error
+	After(tx *sql.Tx, result []interface{}) error
+	OnError(tx *sql.Tx, err error) error
+}
+
+// GlobalInterceptors is the registry of a list of Interceptors that is
+// guaranteed to be executed in its list order on each query.
+var GlobalInterceptors []Interceptor
+
+// LocalInterceptors is the registry for each named query (qID) of a list of
+// Interceptors that is guaranteed to be executed in its list order on each
+// query.
+var LocalInterceptors = make(map[string][]Interceptor)
