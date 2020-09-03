8199f67849f2b50af965b20ce44ce6d6118d7614
Qian Chen <elgs1980@hotmail.com>
2020-09-03 15:13:40 -0700

    Update.

--- interceptors/verify-user.go
+++ interceptors/verify-user.go
@@ -2,12 +2,22 @@ package interceptors
 
 import (
 	"database/sql"
+	"errors"
 
 	"github.com/elgs/wsl"
 )
 
 type VerifyUserInterceptor struct {
 	*wsl.DefaultInterceptor
+}
+
+func (this *VerifyUserInterceptor) Before(tx *sql.Tx, context map[string]interface{}) error {
+
+	if _, ok := context["session"].(map[string]interface{}); !ok {
+		return errors.New("invalid_session")
+	}
+
+	return nil
 }
 
 func (this *VerifyUserInterceptor) After(tx *sql.Tx, context map[string]interface{}, results *interface{}, allResults interface{}) error {
